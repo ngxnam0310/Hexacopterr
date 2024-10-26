54f7d8d1d8835f1ccb0977d0f82ca93e5463ac4b
ngxnam0310 <ngxnamapp03@gmail.com>
2024-10-26 12:12:20 +0700

    LEDs

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include <Arduino.h>
-#Hello
+//Hello
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  int x;
-  double y;
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+
+  delay(4000);
+
+  digitalWrite(5, LOW);
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
