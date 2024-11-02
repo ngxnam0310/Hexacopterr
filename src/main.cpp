e94d04dd9356500ee82f437c9c0c3c1e82cbf7dc
ngxnam0310 <ngxnamapp03@gmail.com>
2024-11-02 20:54:48 +0700

    Code is good but not the pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,23 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <I2Cdev.h>
+#include <MPU6050.h>
 //Hello
 // put function declarations here:
 int myFunction(int, int);
 
-float RateRoll, RatePitch, RateYaw;
-void gyro_signals(void){
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1A);
-  Wire.write(0x05);
-  Wire.endTransmission();
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
-  Wire.beginTransmission(0x68);
-  Wire.write(0x1B); 
-  Wire.write(0x8); 
-  Wire.endTransmission();
+struct MyData {
+  byte X;
+  byte Y;
+  byte Z;
+};
 
-  Wire.beginTransmission(0x68);
-  Wire.write(0x43);
-  Wire.endTransmission();
-  Wire.requestFrom(0x68,6);
+MyData data;
 
-  int16_t GyroX=Wire.read()<<8 | Wire.read();
-  int16_t GyroY=Wire.read()<<8 | Wire.read();
-  int16_t GyroZ=Wire.read()<<8 | Wire.read();
-
-  RateRoll = float(GyroX / 65.5);
-  RatePitch = float(GyroY / 65.5);
-  RateYaw = float(GyroZ / 65.5);
-}
 
 /*Measuring Voltage*/
 /*float Voltage;
@@ -54,15 +43,8 @@ void setup() {
 
   /*Measuring angles with Gyro*/
   Serial.begin(57600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  Wire.setClock(400000);
   Wire.begin();
-  delay(250);
-  Wire.beginTransmission(0x68);
-  Wire.write(0x6B);
-  Wire.write(0x00);
-  Wire.endTransmission();
+  mpu.initialize();
 }
 
 void loop() {
@@ -72,14 +54,20 @@ void loop() {
   Serial.println("V");
   delay(50);*/
 
-  gyro_signals();
-  Serial.print("Roll rate [°/s]= ");
-  Serial.print(RateRoll);
-  Serial.print(" Pitch Rate [°/s]= ");
-  Serial.print(RatePitch);
-  Serial.print("Yaw rate [°/s]= ");
-  Serial.print(RateYaw);
-  delay(50);
+  /*Measuring angles via gyroscope*/
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  data.X = map(ax, -17000, 17000, 0, 255 ); // X axis data
+  data.Y = map(ay, -17000, 17000, 0, 255); 
+  data.Z = map(az, -17000, 17000, 0, 255);  // Y axis data
+  delay(500);
+  Serial.print("Axis X = ");
+  Serial.print(data.X);
+  Serial.print("  ");
+  Serial.print("Axis Y = ");
+  Serial.print(data.Y);
+  Serial.print("  ");
+  Serial.print("Axis Z  = ");
+  Serial.println(data.Z);
 }
 
 
